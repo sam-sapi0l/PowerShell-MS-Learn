db13d7eaf1f57a083348175580b4d5bc55fb3ace
Gabriel Dallemole <th3g3ntl3m3n@duck.com>
2024-04-30 10:07:46 -0300

    finalizado conteudo 3

--- Modulo-2/Conteudo-Modulo-2/conteudo-3-modulo-2.ps1
+++ Modulo-2/Conteudo-Modulo-2/conteudo-3-modulo-2.ps1
@@ -5,3 +5,21 @@ $PSVersionTable
 
 # Imprime versão do PowerShell
 $PSVersionTable.PSVersion
+
+# Obtendo ajuda no PowerShell
+Get-Help -Name Get-FileHash
+
+# help command
+help Get-FileHash
+
+# 2° Output
+help Get-FileHash -Examples
+
+# Descobrindo Objetos utilizando Get-Member
+Get-Process -Name 'nome-do-processo' | Get-Member
+
+# Select-Object commandlet
+Get-Process -Name 'nome-do-processo' | Get-Member | Select-Object Name, MemberType
+
+# Get-Process irá imprimir na tela todos os processos ativos no PC
+Get-Process
