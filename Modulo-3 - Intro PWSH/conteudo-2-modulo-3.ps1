205b1503d964fcaa40779ba9722268fca3120a42
Gabriel Dallemole <th3g3ntl3m3n@duck.com>
2024-05-01 09:25:45 -0300

    Ajustes nas pastas e inicio de novo modulo

---
+++ Modulo-3 - Intro PWSH/conteudo-2-modulo-3.ps1
@@ -0,0 +1,21 @@
+# Connect Commands indto a pipeline
+# Get-Member retorna informação sobre objeto como: 
+Get-Process | Get-Member
+
+# Getting the full response
+Get-Process zsh | Format-List -Property *
+
+# Selecionando colunas específica
+Get-Process zsh | Get-Member zsh -Name C*
+
+# Melhorando output de consulta 
+Get-Process zsh | Select-Object -Property Id, Name, CPU
+
+# Sorting Object
+Get-Process | Sort-Object -Descending -Property -Name
+
+# Selecionando mais de uma propriedade de um objeto
+Get-Process | Sort-Object -Descending -Property Name, CPU
+
+# Expressão customizada
+Get-Process 'powershell' | Sort-Object -Property@(Expression = "Name"; )
